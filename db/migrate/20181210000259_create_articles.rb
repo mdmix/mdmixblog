66e2719b42d69f44db250f98df2bc62df2bd45fe
mdmix <max@maxdiamondmix.com>
2018-12-10 01:18:38 +0100

    basic scaffolding set up

---
+++ db/migrate/20181210000259_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+      t.string :contributor
+      t.string :image_url
+      t.timestamp :time
+
+      t.timestamps
+    end
+  end
+end
